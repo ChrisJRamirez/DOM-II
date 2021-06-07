6f5e6fda83d1e6baf49b3ccf49c20a39a06a07e6
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-06-06 23:28:26 -0400

    8 events added and working

--- js/index.js
+++ js/index.js
@@ -44,7 +44,62 @@ homeFocus.addEventListener("focus", event => {
     event.target.style.background = "orange"
 });
 
-// 6) resize event
+// // 6) resize event
+// window.addEventListener("resize", event => {
+//     var w = document.documentElement.clientWidth;
+//     var h = document.documentElement.clientHeight;
+//     alert(`Width:${w} Height: ${h}`);
+
+// });
+
+// 7) scroll event
+
+
+// const arrowTop = document.createElement("div");
+// arrowTop.onclick = function () {
+//     window.scrollTo(pageXOffset, 0);
+// };
+
+// //css components of arrow div
+// arrowTop.style.height = "9px";
+// arrowTop.style.width = "14px";
+// arrowTop.style.color = "green";
+// arrowTop.style.position = "fixed";
+// arrowTop.style.top = "10px";
+// arrowTop.style.left = "10px";
+// arrowTop.style.cursor = "pointer";
+// arrowTop.textContent = '▲';
+
+// document.querySelector("body").prepend(arrowTop);
+
+// window.addEventListener('scroll', function () {
+//     arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+// });
+
+// console.log(arrowTop)
+
+// 8) select event
+let inputButton = document.createElement("input");
+inputButton.value = "this is nice";
+let inputPara = document.createElement("p");
+const textContentDiv = document.querySelector(".text-content");
+textContentDiv.prepend(inputButton, inputPara)
+
+document.querySelector("input").addEventListener("select", event => {
+    console.log(event);
+    const selectedItems = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    inputButton.value = selectedItems;
+    inputButton.style.color = "orange"
+});
+
+
+// 9) dbl click event
+const doubleHeader = document.querySelector("h2");
+//console.log(doubleHeader)
+doubleHeader.addEventListener("dblclick", event => {
+    doubleHeader.style.color = "orange"
+});
+
 
 
 
